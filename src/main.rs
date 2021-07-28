f541a04439a41ad6512fc71c4f9332a1ed25ec33
Luke Pearson <pearson.luke@gmail.com>
2021-07-28 21:24:50 +0100

    Tidying up

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 extern crate find_folder;
 extern crate piston_window;
 
-use std::fmt::format;
-
 use piston_window::*;
 
 const WIDTH: usize = 160;
 const HEIGHT: usize = 160;
+const SIZE: usize = 5;
+const WW: usize = WIDTH - 1;
+const HH: usize = HEIGHT - 1;
 
 fn main() {
-    let help_text = ["\"Space\" to pause", "\"Esc\" to quit", "\"r\" to reset", "\"-\" to reduce speed", "\"+\" to increase speed"];
+    let help_text = [
+        "\"Space\" to pause",
+        "\"Esc\" or \"q\" to quit",
+        "\"r\" to reset",
+        "\"-\" to reduce speed",
+        "\"+\" to increase speed",
+    ];
     let mut fps = 10;
 
     let opengl = OpenGL::V3_2;
@@ -30,17 +37,12 @@ fn main() {
     let mut events = Events::new(EventSettings::new());
     events.set_max_fps(fps);
 
-    let size = 5;
-    let ww = WIDTH - 1;
-    let hh = HEIGHT - 1;
-
     let mut cells = generate_random();
     let mut prev: [[bool; WIDTH]; HEIGHT] = [[true; WIDTH]; HEIGHT];
 
     let mut new_cells = cells.clone();
-    let rect = math::margin_rectangle([0.0, 0.0, size as f64, size as f64], 1.0);
+    let rect = math::margin_rectangle([0.0, 0.0, SIZE as f64, SIZE as f64], 1.0);
     let red = [1.0, 0.0, 0.0, 0.4];
-    let mut restart = false;
     let mut paused = false;
     let mut frame = 0;
     let mut show_text = true;
@@ -48,31 +50,23 @@ fn main() {
     while let Some(e) = events.next(&mut window) {
         if let Some(Button::Keyboard(key)) = e.press_args() {
             if key == Key::R {
-                restart = true
+                cells = generate_random();
+                frame = 0;
             }
             if key == Key::Space {
                 paused = !paused;
             }
+            if key == Key::Q {
+                return;
+            }
             if key == Key::Plus || key == Key::Equals {
-                fps += 1;
-                if fps > 60 {
-                    fps = 60;
-                }
+                fps = if fps >= 60 { 60 } else { fps + 1 };
                 events.set_max_fps(fps);
             }
             if key == Key::Minus {
-                fps -= 1;
-                if fps < 1 {
-                    fps = 1;
-                }
+                fps = if fps <= 1 { 1 } else { fps - 1 };
                 events.set_max_fps(fps);
             }
-            continue;
-        };
-
-        if restart == true {
-            cells = generate_random();
-            restart = false;
         }
 
         window.draw_2d(&e, |c, g, device| {
@@ -92,11 +86,11 @@ fn main() {
 
             if show_text == true {
                 let mut text_index = 1.0;
-                let starting_y = hh / 2;
+                let starting_y = HH / 2;
                 for text in help_text.iter() {
                     let transform = c
                         .transform
-                        .trans((ww as f64) / 2.0, (20.0 * text_index) + starting_y as f64);
+                        .trans((WW as f64) / 2.0, (20.0 * text_index) + starting_y as f64);
                     text::Text::new_color([0.8, 0.8, 0.8, 1.0], 18)
                         .draw(text, &mut glyphs, &c.draw_state, transform, g)
                         .unwrap();
@@ -109,77 +103,12 @@ fn main() {
 
             for h in 0..HEIGHT {
                 for w in 0..WIDTH {
-                    let c = c.trans(w as f64 * size as f64, h as f64 * size as f64);
+                    let c = c.trans(w as f64 * SIZE as f64, h as f64 * SIZE as f64);
                     if cells[h][w] == true {
                         rectangle(red, rect, c.transform, g);
                     }
 
-                    let up = if h > 0 { cells[h - 1][w] } else { cells[hh][w] };
-                    let right = if w < ww { cells[h][w + 1] } else { cells[h][0] };
-                    let left = if w > 0 { cells[h][w - 1] } else { cells[h][ww] };
-                    let down = if h < hh { cells[h + 1][w] } else { cells[0][w] };
-
-                    let up_left = if h > 0 && w > 0 {
-                        cells[h - 1][w - 1]
-                    } else if w > 0 {
-                        cells[hh][w - 1]
-                    } else if h > 0 {
-                        cells[h - 1][ww]
-                    } else {
-                        cells[hh][ww]
-                    };
-
-                    let up_right = if h > 0 && w < ww {
-                        cells[h - 1][w + 1]
-                    } else if w < ww {
-                        cells[hh][w + 1]
-                    } else if h > 0 {
-                        cells[h - 1][0]
-                    } else {
-                        cells[hh][0]
-                    };
-
-                    let down_left = if h < hh && w > 0 {
-                        cells[h + 1][w - 1]
-                    } else if w > 0 {
-                        cells[0][w - 1]
-                    } else if h < hh {
-                        cells[h + 1][ww]
-                    } else {
-                        cells[0][ww]
-                    };
-
-                    let down_right = if h < hh && w < ww {
-                        cells[h + 1][w + 1]
-                    } else if w < ww {
-                        cells[0][w + 1]
-                    } else if h < hh {
-                        cells[h + 1][0]
-                    } else {
-                        cells[0][0]
-                    };
-
-                    let mut alive = 0;
-                    for direction in &[
-                        up, right, left, down, up_left, up_right, down_left, down_right,
-                    ] {
-                        if direction == &true {
-                            alive += 1;
-                        }
-                    }
-
-                    let mut will_live = cells[h][w];
-                    if alive == 3 {
-                        will_live = true;
-                    }
-                    if alive > 3 {
-                        will_live = false;
-                    }
-                    if alive < 2 {
-                        will_live = false;
-                    }
-
-                    new_cells[h][w] = will_live;
+                    new_cells[h][w] = determine_fate(cells, w, h);
                 }
             }
 
@@ -187,7 +116,6 @@ fn main() {
 
             if prev == new_cells {
                 println!("Ended after {} frames", frame);
-                restart = true;
                 frame = 0;
             }
             if !paused {
@@ -207,4 +135,73 @@ fn generate_random() -> [[bool; WIDTH]; HEIGHT] {
         }
     }
     return cells;
+}
+
+fn determine_fate(cells: [[bool; WIDTH]; HEIGHT], w: usize, h: usize) -> bool {
+    let up = if h > 0 { cells[h - 1][w] } else { cells[HH][w] };
+    let right = if w < WW { cells[h][w + 1] } else { cells[h][0] };
+    let left = if w > 0 { cells[h][w - 1] } else { cells[h][WW] };
+    let down = if h < HH { cells[h + 1][w] } else { cells[0][w] };
+
+    let up_left = if h > 0 && w > 0 {
+        cells[h - 1][w - 1]
+    } else if w > 0 {
+        cells[HH][w - 1]
+    } else if h > 0 {
+        cells[h - 1][WW]
+    } else {
+        cells[HH][WW]
+    };
+
+    let up_right = if h > 0 && w < WW {
+        cells[h - 1][w + 1]
+    } else if w < WW {
+        cells[HH][w + 1]
+    } else if h > 0 {
+        cells[h - 1][0]
+    } else {
+        cells[HH][0]
+    };
+
+    let down_left = if h < HH && w > 0 {
+        cells[h + 1][w - 1]
+    } else if w > 0 {
+        cells[0][w - 1]
+    } else if h < HH {
+        cells[h + 1][WW]
+    } else {
+        cells[0][WW]
+    };
+
+    let down_right = if h < HH && w < WW {
+        cells[h + 1][w + 1]
+    } else if w < WW {
+        cells[0][w + 1]
+    } else if h < HH {
+        cells[h + 1][0]
+    } else {
+        cells[0][0]
+    };
+
+    let mut alive = 0;
+    for direction in &[
+        up, right, left, down, up_left, up_right, down_left, down_right,
+    ] {
+        if direction == &true {
+            alive += 1;
+        }
+    }
+
+    let mut will_live = cells[h][w];
+    if alive == 3 {
+        will_live = true;
+    }
+    if alive > 3 {
+        will_live = false;
+    }
+    if alive < 2 {
+        will_live = false;
+    }
+
+    will_live
 }
